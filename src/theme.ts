cdae408bb356ecca68f89ebef4403e67968695f0
venkata-hari <xprogrammer.me@gmail.com>
2025-06-03 22:05:20 +0530

    fixed all bugs

--- src/theme.ts
+++ src/theme.ts
@@ -1,22 +1,6 @@
 import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
-    palette: {
-        background: {
-          default:'#FDF7E1', 
-        },
-    },
-    typography: {
-      fontFamily: 'Lato',
-        fontSize: 12,
-      h5: {
-      fontSize: '1.5rem',
-    },  
-
-     button: {
-      fontSize: '0.875rem',
-      textTransform: 'none', // Optional: removes UPPERCASE from buttons
-    },
-    },
+  
     
   });
 export default theme
