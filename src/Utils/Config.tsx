8fcd10f8303406bd5128b087d9cf532066707d80
venkata-hari <xprogrammer.me@gmail.com>
2025-07-12 17:48:57 +0530

    modified apis

--- src/Utils/Config.tsx
+++ src/Utils/Config.tsx
@@ -1,12 +1,10 @@
 export const baseURL='https://8257c02b0c13.ngrok-free.app/api'
 export const endpoints:any={
-
     //Adminlogin
     SIGNIN:'auth/signin',
     FORGETPASSWORD:'auth/forgetpassword',
     VERIFYOTP:'auth/verifyotp',
     CHANGEPASSWORD:'auth/changepassword',
-
     //AdminGallary
     CREATEGALLARY:'gallery/create_gallery',
     GETGALLARY:'gallery/all_galleries',
